fefb59e8784c3d5bb0ea3b089570df34efe39b58
shanmukhdutt <ksssaidutt@gmail.com>
2024-02-29 11:08:12 +0530

    Improved the local storage

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -122,7 +122,7 @@ export default defineComponent({
       };
       users.value.push(newUser);
       localStorage.setItem('users',JSON.stringify(users.value));
-      clearInputFields();
+      clearInputFields()
       closeModal();
     };
 
